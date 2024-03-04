781b7b29184605d175e7bb5d01e26185f8c90c05
Daikel <daikelcruzgonzalez48@gmail.com>
2024-03-04 16:23:51 +0000

    mejora

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const skillBars = document.querySelectorAll('.skill-bar');
-  
-    // Función para animar las barras de habilidades
-    function animateSkillBars() {
-      skillBars.forEach(skillBar => {
-        const level = skillBar.dataset.level;
-        skillBar.style.width = level; // Asigna el ancho de la barra según el nivel de habilidad
-      });
-    }
-  
-    // Ejecuta la función de animación cuando se carga la página
-    animateSkillBars();
-  });
-  
+// /* Cuando hago CLICK .button, .nav TOGGLE 'activo' */
+const button = document.querySelector('.button')
+const nav    = document.querySelector('.nav')
+
+button.addEventListener('click',()=>{
+    nav.classList.toggle('activo')
+})
+
